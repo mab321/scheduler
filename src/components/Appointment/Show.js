f191c1f1634cba072e00866d8757842a73e6bc88
mab321 <monther.balghonaim@gmail.com>
2022-04-04 00:19:42 -0700

    Added Form, show and implemented show in index of appointments

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -7,7 +7,7 @@ import React from "react";
     onDelete:Function to be called when the user clicks the Delete button
 */
 export default function Show(props) {
-   
+   console.log(props.interviewer.name);
 
    return (
     <main className="appointment__card appointment__card--show">
@@ -15,7 +15,7 @@ export default function Show(props) {
       <h2 className="text--regular">{props.student}</h2>
       <section className="interviewer">
         <h4 className="text--light">Interviewer</h4>
-        <h3 className="text--regular">{props.interviewer}</h3>
+        <h3 className="text--regular">{props.interviewer.name}</h3>
       </section>
     </section>
     <section className="appointment__card-right">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Fragment } from 'react'
+
 
 import "./style.scss";
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 export default function Appointment(props) {
    
 
    return (
-    <article className="appointment">{props.time ? `Appointment at ${props.time}`: "No Appointment"}</article>
+    <article className="appointment">
+       <Header time={props.time} />
+       <Fragment>
+          
+          {props.interview ?
+          <>
+           <Show student={props.interview.student} interviewer ={props.interview.interviewer}/>
+           </>
+         : 
+           <>
+           <Empty onAdd={()=> console.log("empty")}/>
+           </>
+           }
+           
+       </Fragment>
+       </article>
 
    );
 }
