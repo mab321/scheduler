c2dd64136c345b10710416b1408d8c9469ae5925
mab321 <monther.balghonaim@gmail.com>
2022-04-04 16:47:57 -0700

    Added axios and use affect  to set daylist days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 
 
 import "components/Application.scss";
@@ -45,23 +46,15 @@ const appointments = {
 
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  
+  const [days,setDays] = useState([])
+  useEffect(() => {
+    
+    axios.get("/api/days").then(response => {
+      setDays((prev) => response.data )
+      console.log(response);
+    });
+  }, []);
   const [day,setDay] = useState("Monday");
   const appointmentList = Object.values(appointments).map((appointment) => {
     return (
