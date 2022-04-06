f068ad61ce4a53f7d36727d55343e655fa19cdbf
mab321 <monther.balghonaim@gmail.com>
2022-04-06 03:24:45 -0700

    implemented error handling for ERROR_SAVE and ERROR_DELETE modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from 'hooks/useVisualMode';
 export default function Appointment(props) {
 
@@ -18,6 +19,8 @@ export default function Appointment(props) {
    const CONFIRM = "CONFIRM";
    const DELETING = "DELETING";
    const EDIT = "EDIT";
+   const ERROR_SAVE = "ERROR_SAVE";
+   const ERROR_DELETE = "ERROR_DELETE";
 
 
    const { mode, transition, back } = useVisualMode(
@@ -33,7 +36,7 @@ export default function Appointment(props) {
        props.bookInterview(props.id, interview)
         .then(() => transition(SHOW))
         .catch((error) => {
-           console.log("bookInterview",error);
+           transition(ERROR_SAVE, true);
         })
 
     }
@@ -44,7 +47,7 @@ export default function Appointment(props) {
           transition(EMPTY);
        })
        .catch((error) => {
-          console.log("DeleteInterview", error);
+          transition(ERROR_DELETE, true);
        })
 
     }
@@ -83,6 +86,15 @@ export default function Appointment(props) {
         {mode === SAVING && <Status message={"SAVING"}/>}
         {mode === CONFIRM && <Confirm  onConfirm={DeleteInterview} onCancel={() => transition(SHOW)}/>}
         {mode === DELETING && <Status message={"DELETING"}/>}
+        {mode === ERROR_SAVE && (
+        <Error
+         message={"Server could not save appointment"}
+          onClose={back}/>)}
+          
+        {mode === ERROR_DELETE && (
+        <Error 
+        message={"Server could not delete appointment"} 
+        onClose={back}/>)}
            
        </Fragment>
        </article>
