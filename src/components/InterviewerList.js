e2226ecb37638e56d45e92dcdd3bb0921a42c3e0
mab321 <monther.balghonaim@gmail.com>
2022-04-07 06:45:03 -0700

    implemented prop-types library in interviewlist

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types'; 
 export default function InterviewerList(props){
   console.log("interViewer list props",props);
   
@@ -25,3 +26,7 @@ export default function InterviewerList(props){
 
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
