413811a2309eaaba83e3d5d501a567385b842c2b
mab321 <monther.balghonaim@gmail.com>
2022-04-09 17:46:19 -0700

    Added cypress test cases for the navigation and the crud of appointments

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,57 @@
+
+
+describe("Booking", () => {
+  beforeEach(() => {
+    // reset db
+    cy.request("GET","/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  })
+
+  it("should book an interview", () => {
+
+    cy.get("[alt=Add]").first().click();
+  
+   // set student name, choose interviewer and save
+    cy.get("[data-testid=student-name-input]")
+      .type("Luke Skywalker");
+    
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains('Save').click();
+    
+    // Confirm booked appointment
+    cy.contains(".appointment__card--show","Luke Skywalker" );
+    
+
+  });
+  
+  it("Should edit interview", () => {
+    // force click edit button
+    cy.get("[alt='Edit']")
+      .click({force: true});
+    
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("chewbacca");
+    
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains('Save').click();
+    // check that both student name and interviewer were edited
+    cy.contains(".appointment__card--show","chewbacca" );
+    cy.contains(".appointment__card--show","Tori Malcolm" );
+
+  });
+ 
+  it("should cancel an interview", () => {
+    // get delete button
+    cy.get("[alt='Delete']")
+      .click({force: true}); // forcing a click overrides the actionable checks of cypress and fire automatically
+
+    cy.contains("Confirm").click();
+  // check that the first appointment is deleted
+    cy.contains(".appointment__card--show","Archie Cohen" ).should("not.exist");
+
+  });
+})
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -9,10 +9,14 @@ describe("Navigation", () => {
 
     cy.contains("li", "Tuesday")
     .should("have.css", "background-color", "rgb(242, 242, 242)");
-    */
+    
     cy.contains("li", "Tuesday")
     .click()
     .should("have.css", "background-color", "rgb(242, 242, 242)");
+    */
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
   });
   
 });
